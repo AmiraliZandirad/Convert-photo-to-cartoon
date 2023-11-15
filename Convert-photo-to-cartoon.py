fd8b9d3817c8e3620b64f2b0b4aac087957e9e0e
AmirAli Zandi Rad <138608178+AmiraliZandi@users.noreply.github.com>
2023-11-15 22:13:37 +0330

    Add files via upload

---
+++ Convert-photo-to-cartoon.py
@@ -0,0 +1,41 @@
+# Generat by  AmiraliZandi 
+#    ===                             =                 ===      ==       =
+#  =     =   ==   ===      ===             ==   =    =     =    ==  
+#  =======   ==  =   =   =    =     ==     == =      =======    ==       ==
+#  =     =   == =     = =      =    ==     ==        =     =    ==       ==
+#  =     =   ==        =       =    ==     ==        =     =    ======   ==
+
+#**************************************************************************************************
+import cv2
+import numpy as np
+
+
+def cartoonize_image(image):
+   
+    filtered = cv2.bilateralFilter(image, 9, 250, 250)
+
+    
+    gray = cv2.cvtColor(filtered, cv2.COLOR_BGR2GRAY)
+
+
+    edges = cv2.adaptiveThreshold(gray, 255,
+                                  cv2.ADAPTIVE_THRESH_MEAN_C,
+                                  cv2.THRESH_BINARY, 9, 2)
+
+    
+    color = cv2.bilateralFilter(image, 8, 250, 250)
+
+
+    cartoon = cv2.bitwise_and(color, color, mask=edges)
+
+    return cartoon
+
+
+
+image = cv2.imread("R.jpeg")
+cartoonized = cartoonize_image(image)
+
+
+cv2.imshow("Cartoonized Image", cartoonized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
